052260caa807f3995d1d51a43030ac895e5b8a44
孙航 <2662419405@qq.com>
2020-05-18 09:14:45 +0800

    独立提取React和ReactDOM

---
+++ config/cdn.js
@@ -0,0 +1,10 @@
+module.exports = {
+  cdn: {
+    css: ["http://at.alicdn.com/t/font_1623818_h2oo6f8mmgu.css"],
+    js: [
+      "http://api.map.baidu.com/api?v=2.0&ak=MxmWREMS7YGrnyDU4PNfbizZSAeMRGNB",
+      "https://unpkg.com/react@16/umd/react.production.min.js",
+      "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js",
+    ],
+  },
+};
